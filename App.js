84c0bee9b769bf5fc7ac5bd41bda46c1a4f7a0a9
nickthorpe71 <nickthorpe71@gmail.com>
2021-04-24 16:02:44 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Header />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 60,
+  },
+});
+
+export default App;
---
+++ components/Header.js
@@ -0,0 +1,26 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Header = () => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.text}>Blendr</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    height: 60,
+    padding: 15,
+    backgroundColor: 'darkslateblue',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 23,
+    textAlign: 'center',
+  },
+});
+
+export default Header;

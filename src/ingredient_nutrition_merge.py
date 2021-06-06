124ce6e848716de8f2ea6767220d0e75f8a04397
nickthorpe71 <nickthorpe71@gmail.com>
2021-06-05 21:48:41 -0500

    add script to add nutrition to ingredients

--- src/ingredient_nutrition_merge.py
+++ src/ingredient_nutrition_merge.py
@@ -1,14 +1,24 @@
 import json
 
-
 with open('ingredientsNoNutrition.json', 'r') as ingredients:
     ingredient_data = json.load(ingredients)
 
-with open('masterBulkIngredients.json', 'r') as nutrition:
+with open('masterNutrition.json', 'r') as nutrition:
     nutrition_data = json.load(nutrition)
 
+print(ingredient_data)
+
+allIngredients = []
+
 for ingredient in ingredient_data:
+    nutrition = {}
+
+    for item in nutrition_data:
+        if item.get("name") == ingredient.get("pullNutritionFrom"):
+            ingredient["nutrition"] = item
+
+    allIngredients.append(ingredient)
 
 
-with open('ingredientsNoNutrition.json', 'w') as f:
+with open('ingredientsWithNutrition.json', 'w') as f:
     json.dump(allIngredients, f, indent=2)
